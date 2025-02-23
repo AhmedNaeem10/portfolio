9eac29d6e0312ec55145eeeddd7b9db3a793fd02
Ahmed Naeem <k191346@nu.edu.pk>
2025-02-23 20:31:58 +0500

    base porfolio

---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+    name: "Ahmed Naeem",
+    profile: '/profile.png',
+    designation: "Software Developer",
+    description: "My name is Ahmed Naeem. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+    email: 'ahmednaeem.career@gmail.com',
+    phone: '+923315371989',
+    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
+    github: 'https://github.com/AhmedNaeem10',
+    facebook: 'https://www.facebook.com/ahmad.naeem.9026040',
+    linkedIn: 'https://www.linkedin.com/in/ahmed-naeem-2654bb209/',
+    twitter: '',
+    stackOverflow: 'https://stackoverflow.com/users/12278826/ahmed-naeem',
+    leetcode: "",
+    devUsername: "ahmednaeem5575",
+    resume: "https://drive.google.com/file/d/18uhvQ614kHGZRcnNlXT8-yx8_NCzQqzu/view?usp=sharing"
+  }
